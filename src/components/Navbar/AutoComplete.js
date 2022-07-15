881267692142701362d43355e8f5bea63de5ca28
Michael-A-I <zero.aduayi@gmail.com>
2022-07-15 16:04:15 -0400

    downgrade react change algolia search to use react 17 pattern

--- src/components/Navbar/AutoComplete.js
+++ src/components/Navbar/AutoComplete.js
@@ -1,11 +1,9 @@
 import { autocomplete } from "@algolia/autocomplete-js"
 import React, { createElement, Fragment, useEffect, useRef } from "react"
-import { createRoot } from "react-dom/client"
+import { render } from "react-dom"
 
-export function Autocomplete(props) {
+function Autocomplete(props) {
   const containerRef = useRef(null)
-  const panelRootRef = useRef(null)
-  const rootRef = useRef(null)
 
   useEffect(() => {
     if (!containerRef.current) {
@@ -14,17 +12,7 @@ export function Autocomplete(props) {
 
     const search = autocomplete({
       container: containerRef.current,
-      renderer: { createElement, Fragment, render: () => {} },
-      render({ children }, root) {
-        if (!panelRootRef.current || rootRef.current !== root) {
-          rootRef.current = root
-
-          panelRootRef.current?.unmount()
-          panelRootRef.current = createRoot(root)
-        }
-
-        panelRootRef.current.render(children)
-      },
+      renderer: { createElement, Fragment, render },
       ...props
     })
 
